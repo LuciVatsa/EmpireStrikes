0f8f61e75d8a71ae88dc7d931c21e397225a8bba
Mitch <mitch@pop-os.localdomain>
2020-10-19 18:58:03 -0600

    creates Gravity Ball level

---
+++ EmpireStrikes/Assets/Scripts/GravityRoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityRoom : MonoBehaviour {
+    // Override
+    void Update() {
+        float rotateX = 0f;
+        float rotateZ = 0f;
+        float rotateSpeed = 1f;
+
+        if (Input.GetKey(KeyCode.W)) {
+            rotateX -= rotateSpeed;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            rotateX += rotateSpeed;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            rotateZ -= rotateSpeed;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            rotateZ += rotateSpeed;
+        }
+
+        transform.Rotate(rotateX, 0f, rotateZ);
+    }
+}
+
