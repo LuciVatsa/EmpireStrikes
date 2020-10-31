08488de789cb9868a36860a98bb970132f7a7d44
MitchellJAllen <mitchelljohnallen1@gmail.com>
2020-10-31 01:03:14 -0600

    adds buttons, prototypes look-to-press functionality

---
+++ EmpireStrikes/Assets/Scripts/GravityRotateFront.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityRotateFront : GravityButtonTrigger {
+    public override void OnPress() {
+        transform.Rotate(Vector3.right, -30f * Time.deltaTime, Space.World);
+    }
+}
