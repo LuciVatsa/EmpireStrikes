3e9fc4fc49a1f15e7aa1dd0f48e4763d29fa14ac
Rud156 <rudrangshu_1997@yahoo.com>
2020-11-04 16:27:38 -0700

    Merge

---
+++ EmpireStrikes/Assets/Scenes/MiniGame-Escape Room/Scripts/TeleportPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(door.GetComponent<DoorScript>().getDoorStatus())
+        {
+            float xDist = Mathf.Abs(transform.position.x - player.transform.position.x);
+            float yDist = Mathf.Abs(transform.position.y - player.transform.position.y);
+            float zDist = Mathf.Abs(transform.position.z - player.transform.position.z);
+            if (xDist < 5 && yDist < 5 && zDist < 5)
+            {
+                player.transform.position = new Vector3(71, 35, -23);
+            }
+        }
+    }
+
+
+    
+
+}
