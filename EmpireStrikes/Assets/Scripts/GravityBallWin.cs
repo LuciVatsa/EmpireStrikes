4ffd063ab5fccf4c87f4a9379d2e7a06162ed462
Mitch <mitch@pop-os.localdomain>
2020-12-04 21:20:02 -0700

    implements basic win condition detection

---
+++ EmpireStrikes/Assets/Scripts/GravityBallWin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBallWin : MonoBehaviour {
+    // Override
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("WIN");
+    }
+}
+
